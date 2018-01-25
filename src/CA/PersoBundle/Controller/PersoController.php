21f4504a0fb6e66082235b7539f6a37efa794c37
thefab96 <locqueville.fabrice@gmail.com>
2018-01-25 17:22:10 +0100

    ajout d'un commentaire

--- src/CA/PersoBundle/Controller/PersoController.php
+++ src/CA/PersoBundle/Controller/PersoController.php
@@ -81,10 +81,10 @@ class PersoController extends Controller
         $em = $this->getDoctrine()->getManager();
         $friend = $em->getRepository('CAPersoBundle:Perso')->find($id);
 
+        //JUSTE POUR L'EXEMPLE si on ajoute deux fois ca bug
         $this->getUser()->addMyFriend($friend);
         $em->flush();
-
-
+        //JUSTE POUR L'EXEMPLE si on ajoute deux fois ca bug
 
         return $this->redirectToRoute('homepage');
     }
